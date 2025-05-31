9c81f2358251ea4ab463a33180fcad96802eb8fa
Josuesp1620 <josuesp1620@gmail.com>
2025-05-30 20:36:36 -0500

    [Update] New Feature Add Source WMS Geoserver in Map Component

--- app/src/main/java/com/geosolution/geoapp/presentation/screens/map/components/Map.kt
+++ app/src/main/java/com/geosolution/geoapp/presentation/screens/map/components/Map.kt
@@ -20,6 +20,9 @@ import org.maplibre.android.geometry.LatLng
 import org.maplibre.android.maps.MapView
 import org.maplibre.android.plugins.annotation.SymbolManager
 import org.maplibre.android.plugins.annotation.SymbolOptions
+import org.maplibre.android.style.layers.RasterLayer
+import org.maplibre.android.style.sources.RasterSource
+import org.maplibre.android.style.sources.TileSet
 import org.maplibre.android.utils.BitmapUtils
 
 @Composable
@@ -58,8 +61,33 @@ fun MarkerMapScreen(
                     Toast.makeText(context, "Opera house", Toast.LENGTH_LONG).show()
                     true
                 }
-            }
+                val wmsUrlTemplate = "https://geosdot.servicios.gob.pe/geoserver/geoportal/wms?" +
+                        "service=WMS" +
+                        "&version=1.1.1" +
+                        "&request=GetMap" +
+                        "&layers=geoportal%3Aaerodromo_2022" +
+                        "&styles=" +
+                        "&format=image%2Fpng" +
+                        "&transparent=true" +
+                        "&srs=EPSG%3A3857" +
+                        "&bbox={bbox-epsg-3857}" +
+                        "&width=256&height=256"
 
+                val sourceId = "wms-source"
+                val layerId = "wms-layer"
+
+                if (style.getSource(sourceId) == null) {
+                    val tileSet = TileSet("tileset", wmsUrlTemplate)
+                    val wmsSource = RasterSource(sourceId, tileSet, 256)
+                    style.addSource(wmsSource)
+                }
+
+                if (style.getLayer(layerId) == null) {
+                    val rasterLayer = RasterLayer(layerId, sourceId)
+                    style.addLayer(rasterLayer)
+                }
+
+            }
             mapboxMap.cameraPosition = CameraPosition.Builder().target(sydney).zoom(13.0).build()
         }
     }
